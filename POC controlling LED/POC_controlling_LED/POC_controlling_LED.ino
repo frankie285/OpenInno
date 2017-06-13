c94b9316af1caba838e0e76ccf7766256a8a67b0
frankie285 <frankievanmeel@hotmail.com>
2017-06-13 12:39:06 +0200

    added commentary to POC controlling LED and simplified GPS POC.

--- POC controlling LED/POC_controlling_LED/POC_controlling_LED.ino
+++ POC controlling LED/POC_controlling_LED/POC_controlling_LED.ino
@@ -1,21 +1,27 @@
+/*
+In dit proof op concept wordt er een ledstrip aangestuurd vanuit een Arduino. De ws2812b ledstrip bevat 3 draden: +, -, data. 
+Om dit proof of concept te laten werken, wordt de + (rood) op de 5V aangesloten, de - (zwart) wordt op GND aangesloten en tot slot wordt de data (groen) op digital pin 5 aangesloten.
+De volgende code zorgt ervoor dat er 5 leds aan gaan en de kleur wit krijgen. vervolgens kan met de functie setColor de RGB waardes van de leds aangepast worden.
+*/
+
 #include "FastLED.h"
 
-#define NUM_LEDS 5
-#define DATA_PIN 6
-CRGB leds[5];
+#define NUM_LEDS 5 //aantal leds aan te sturen
+#define DATA_PIN 6 //pin waarop de data kabel van de strip wordt aangesloten.
+CRGB leds[5]; // ledstrip object.
 
 
 void setup() { 
   FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
   
-  for(int i = 0; i < NUM_LEDS; i++){
+  for(int i = 0; i < NUM_LEDS; i++){ // de 5 leds worden aangezet op wit.
      leds[i] = CRGB(255,255,255); 
   }
     FastLED.show();
 }
 
 void loop() { 
-  setColor(100,0,1);
+  setColor(100,0,1); //in deze functie worden de RGB waardes ingevuld. Dit laat de ledstrip van kleur veranderen.
 }
 
 
--- POC working GPS/POC working GPS.ino
+++ POC working GPS/POC working GPS.ino
@@ -1,3 +1,14 @@
+/*
+In dit proof of concept worden de lattitude en longitude via de GPS sensor opgehaald. DE GPS Sensor EM506 heeft 6 draden van links naar rechts:
+1: LED status
+2: GND
+3: TX
+4: RX
+5: 5V
+6: GND
+zowel pin 2 als 6 moet verbonden zijn met GND, pin 2 met 5V en tot slot de TX op arduino pin 5 en RX op arduino pin 3
+*/
+
 #include <TinyGPS++.h>
 #include <SoftwareSerial.h>
 
@@ -14,6 +25,7 @@ TinyGPSPlus gps;
 
 void setup()
 {
+  Serial.begin(9600);
   gpsSerial.begin(GPSBaud);
 }
 
@@ -30,5 +42,6 @@ void displayInfo()
   {
     lattitude = gps.location.lat();
     longitude = gps.location.lng();
+    Serial.println(lattitude + ':' + longitude);
   }
 }
