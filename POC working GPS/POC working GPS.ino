924391dbd75a12c3eb7667649bf11740e2488d43
frankie285 <frankievanmeel@hotmail.com>
2017-06-13 12:40:07 +0200

    changed POC GPS. the GPS we got from the ISSD broke down, so we got another one.

--- POC working GPS/POC working GPS.ino
+++ POC working GPS/POC working GPS.ino
@@ -6,33 +6,33 @@ In dit proof of concept worden de lattitude en longitude via de GPS sensor opgeh
 4: RX
 5: 5V
 6: GND
-zowel pin 2 als 6 moet verbonden zijn met GND, pin 2 met 5V en tot slot de TX op arduino pin 5 en RX op arduino pin 3
+zowel pin 2 als 6 moet verbonden zijn met GND, pin 2 met 5V en tot slot de TX op arduino pin 5 en RX op arduino pin 3 (pin 3 hoeft niet aangesloten te worden, omdat dit een communicatie naar de gps toe is, die overbodig is)
 */
 
 #include <TinyGPS++.h>
 #include <SoftwareSerial.h>
 
-int RXPin = 3;
-int TXPin = 5;
-SoftwareSerial gpsSerial(RXPin, TXPin);
+static const int RXPin = 3, TXPin = 5; //alleen de TX pin is nodig, er wordt namelijk alleen data uitgelezen.
+static const uint32_t GPSBaud = 4800;
 
-int GPSBaud = 4800;
-
-double lattitude;
-double longitude;
-
+// The TinyGPS++ object
 TinyGPSPlus gps;
+
+SoftwareSerial ss(RXPin, TXPin);
+
+double LAT;
+double LON;
 
 void setup()
 {
   Serial.begin(9600);
-  gpsSerial.begin(GPSBaud);
+  ss.begin(GPSBaud);
 }
 
 void loop()
 {
-  while (gpsSerial.available() > 0)
-    if (gps.encode(gpsSerial.read()))
+  while (ss.available() > 0)
+    if (gps.encode(ss.read()))
       displayInfo();
 }
 
@@ -40,8 +40,11 @@ void displayInfo()
 {
   if (gps.location.isValid())
   {
-    lattitude = gps.location.lat();
-    longitude = gps.location.lng();
-    Serial.println(lattitude + ':' + longitude);
+    LAT = gps.location.lat();
+    LON = gps.location.lng();
+    Serial.print(LAT, 6);
+    Serial.print(" , ");
+    Serial.print(LON, 6);
+    Serial.println();
   }
 }
